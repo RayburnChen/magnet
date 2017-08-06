2842f6cbc033657381ff58ed8b337c66d33a19cd
OwenChen <OwenChen@windows10.microdone.cn>
2017-08-06 19:40:56 +0800

    fix eureka

--- magnet-eureka/src/main/java/org/nfa/eureka/MagnetEurekaApplication.java
+++ magnet-eureka/src/main/java/org/nfa/eureka/MagnetEurekaApplication.java
@@ -19,8 +19,10 @@ package org.nfa.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ WebSecurityConfiguration.class })
 @EnableEurekaServer
 public class MagnetEurekaApplication {
 
