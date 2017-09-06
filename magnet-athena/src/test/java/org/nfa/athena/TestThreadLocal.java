99aa31bcc24fd11fcafd7fcc79597e2775eda383
OwenChen <OwenChen@windows10.microdone.cn>
2017-09-06 21:45:01 +0800

    commit initial thread local

--- magnet-athena/src/test/java/org/nfa/athena/TestThreadLocal.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestThreadLocal.java
@@ -1,11 +1,19 @@
 package org.nfa.athena;
 
+import java.util.Random;
+
 import org.junit.Test;
 import org.nfa.athena.service.Worker;
 
 public class TestThreadLocal {
 	
-	public static final InheritableThreadLocal<String> inherit = new InheritableThreadLocal<String>();
+	public static final InheritableThreadLocal<String> inherit = new InheritableThreadLocal<String>() {
+		@Override
+		protected String initialValue() {
+			Random ran = new Random();
+			return String.valueOf(ran.nextInt());
+		}
+	};
 	public static final ThreadLocal<String> mainThreadLocal = new ThreadLocal<String>();
 
 	@Test
