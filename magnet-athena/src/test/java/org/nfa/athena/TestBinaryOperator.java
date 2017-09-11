04374ce77fbcb89b054fa2dd19ba8a7e6ec219e7
ochen033 <ochen033@OCHEN03306.us.nam.ad.pwcinternal.com>
2017-09-11 10:31:15 +0800

    commit test reflect

---
+++ magnet-athena/src/test/java/org/nfa/athena/TestBinaryOperator.java
@@ -0,0 +1,42 @@
+package org.nfa.athena;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+import org.springframework.data.annotation.Transient;
+
+public class TestBinaryOperator {
+
+	private List<String> list = Arrays.asList("a", "aa", "aaa", "b", "bb", "bbb", "c", "cc", "ccc");
+
+	@Test
+	public void test() {
+		Map<Integer, String> map = list.stream()
+				.collect(Collectors.groupingBy(String::length, Collectors.reducing("", (x, y) -> y)));
+		System.out.println(map.values());
+	}
+	
+	@Test
+	public void testFields() {
+		Field[] dfs = User.class.getDeclaredFields();
+		Field[] fs = User.class.getFields();
+		for (int i = 0; i < dfs.length; i++) {
+			System.out.println("getDeclaredFields" + dfs[i].getName());
+			System.out.println("isAnnotationPresent" + dfs[i].isAnnotationPresent(Transient.class));
+		}
+		for (int i = 0; i < fs.length; i++) {
+			System.out.println("getFields" + fs[i].getName());
+			System.out.println("isAnnotationPresent" + fs[i].isAnnotationPresent(Transient.class));
+		}
+	}
+	
+	@Test
+	public void testFields1() {
+		System.out.println(1 == 3);
+	}
+
+}
