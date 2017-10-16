53fe62097a8bcef7e69500973120ed1258460799
OwenChen <OwenChen@windows10.microdone.cn>
2017-10-16 20:52:16 +0800

    update MagnetAuthApplication

--- magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
+++ magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
@@ -12,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class MagnetAuthApplication {
 
 	// org.springframework.boot.autoconfigure.security.oauth2.authserver.OAuth2AuthorizationServerConfiguration
+	// org.springframework.security.oauth2.provider.endpoint.TokenEndpoint
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetAuthApplication.class, args);
