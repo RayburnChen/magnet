f2850cda51025381e4731fe50b71bdd4600760b6
ochen033 <ochen033@OCHEN03306.us.nam.ad.pwcinternal.com>
2017-10-18 17:08:51 +0800

    add check_token url

--- magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
+++ magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
@@ -17,6 +17,14 @@ public class MagnetAuthApplication {
 	// grant type = password
 	// org.springframework.security.oauth2.provider.password.ResourceOwnerPasswordTokenGranter
 	
+	/*
+	curl magnet-client:passw0rd@localhost:8090/oauth/token -d grant_type=password -d username=user -d password=88350ea4-cbf7-485f-a8ad-8d11ce3d7522 -d scope=write
+
+	curl localhost:8110/greeting/oneUser -H "Authorization: Bearer 9dc9d55a-bbce-46d5-b09d-c505af67df20"
+
+	curl magnet-client:passw0rd@localhost:8090/oauth/check_token -d token=9dc9d55a-bbce-46d5-b09d-c505af67df20
+	*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetAuthApplication.class, args);
 	}
