15fbe022bfb3809dfb2d8e18561f7a0119449f54
ochen033 <ochen033@OCHEN03306.us.nam.ad.pwcinternal.com>
2017-10-18 14:53:54 +0800

    update auth resource server

--- magnet-athena/src/main/java/org/nfa/athena/MagnetAthenaApplication.java
+++ magnet-athena/src/main/java/org/nfa/athena/MagnetAthenaApplication.java
@@ -1,10 +1,8 @@
 package org.nfa.athena;
 
-import org.nfa.athena.config.WebSecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,7 +12,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 //@EnableBinding(MagnetSource.class)
 @EnableResourceServer
-@Import({WebSecurityConfiguration.class})
 public class MagnetAthenaApplication {
 
 	public static void main(String[] args) {
