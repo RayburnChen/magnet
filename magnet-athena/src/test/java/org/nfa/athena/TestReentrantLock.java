1e9876eb05d3288f27dcf7e4687f6f7278158e0f
ochen033 <ochen033@OCHEN03306.us.nam.ad.pwcinternal.com>
2017-10-26 17:25:26 +0800

    add test

---
+++ magnet-athena/src/test/java/org/nfa/athena/TestReentrantLock.java
@@ -0,0 +1,55 @@
+package org.nfa.athena;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.junit.Test;
+
+public class TestReentrantLock {
+
+	private int result = 0;
+	private int count = 100;
+	private ReentrantLock lock = new ReentrantLock();
+
+	@Test
+	public void testReentrantLock() {
+		List<Thread> list = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			Thread th = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					lock.lock();
+					try {
+						int num = result;
+						try {
+							TimeUnit.MILLISECONDS.sleep(10);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						result = num + 1;
+					} finally {
+						lock.unlock();
+					}
+				}
+			});
+			list.add(th);
+		}
+
+		list.forEach(t -> t.start());
+
+		list.forEach(t -> {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+
+		System.out.println("TestReentrantLock.testReentrantLock() " + result);
+
+	}
+
+}
