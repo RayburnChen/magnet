f624a62c977bda33ebbb323169ca502a07f998d2
OwenChen <OwenChen@windows10.microdone.cn>
2017-11-11 18:11:15 +0800

    add some comments

--- magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
+++ magnet-auth/src/main/java/org/nfa/auth/MagnetAuthApplication.java
@@ -36,6 +36,8 @@ public class MagnetAuthApplication {
 	// curl user:qqq@localhost:8090/magnet-auth/oauth/authorize -d client_id=magnet-client -d response_type=code -d redirect_uri=www.baidu.com
 	// localhost:8090/magnet-auth/oauth/authorize?client_id=magnet-client&response_type=code&redirect_uri=http://www.baidu.com
 	
+	// org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint
+	
 	// Client Server Get Authorization Code
 	// Mode 2.2
 	// https://www.baidu.com/?code=SsQH1R
@@ -45,7 +47,7 @@ public class MagnetAuthApplication {
 	// curl magnet-client:passw0rd@localhost:8090/magnet-auth/oauth/token -d grant_type=authorization_code -d redirect_uri=http://www.baidu.com -d code=SsQH1R
 	// Use token to call Resource server
 	
-	// org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint
+	// org.springframework.security.oauth2.provider.endpoint.TokenEndpoint
 	// org.springframework.security.oauth2.provider.code.AuthorizationCodeTokenGranter
 	
 	// Mode 3
@@ -55,6 +57,7 @@ public class MagnetAuthApplication {
 	// localhost:8090/magnet-auth/oauth/authorize?client_id=magnet-client&redirect_uri=http://localhost:8100/magnet-lucia/login&response_type=code&state=RRRwEg
 	// Client Server get token by one call and no need to call Authorization Server again
 	// Implicit will not validate Client Server because there is no Mode 2.3 which need Client Server Secret
+	// Implicit can access token but can not refresh token
 	
 	// org.springframework.security.oauth2.provider.endpoint.AuthorizationEndpoint
 	// org.springframework.security.oauth2.provider.implicit.ImplicitTokenGranter
