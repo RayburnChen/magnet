3cdd39d0b69e71938313300164fe5082d00b4a15
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-11-14 13:03:47 +0800

    add some config

--- magnet-eureka/src/main/java/org/nfa/eureka/WebSecurityConfiguration.java
+++ magnet-eureka/src/main/java/org/nfa/eureka/WebSecurityConfiguration.java
@@ -3,12 +3,18 @@ package org.nfa.eureka;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	private static final String ROLE = "EUREKA_USER";
+	
+//	@Override
+//	public void init(final WebSecurity web) throws Exception {
+//		web.ignoring().anyRequest();
+//	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
