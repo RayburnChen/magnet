b7f7580a8d2d9fff96e20b183a95202c6e5fa659
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-11-14 15:38:36 +0800

    remove eureka security

--- magnet-eureka/src/main/java/org/nfa/eureka/WebSecurityConfiguration.java
+++ magnet-eureka/src/main/java/org/nfa/eureka/WebSecurityConfiguration.java
@@ -1,29 +1,15 @@
 package org.nfa.eureka;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	private static final String ROLE = "EUREKA_USER";
-	
-//	@Override
-//	public void init(final WebSecurity web) throws Exception {
-//		web.ignoring().anyRequest();
-//	}
-
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").hasRole(ROLE).and().httpBasic();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("magnet-client").password("passw0rd").roles(ROLE);
+	public void init(final WebSecurity web) throws Exception {
+		web.ignoring().anyRequest();
 	}
 
 }
