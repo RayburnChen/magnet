ca8294c1748d65dc26d05a0ff4977cdd5cab9046
OwenChen <OwenChen@windows10.microdone.cn>
2017-11-19 13:40:47 +0800

    polish some code

--- magnet-athena/src/test/java/org/nfa/athena/TestCountDownLatch.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestCountDownLatch.java
@@ -8,35 +8,32 @@ import org.junit.Test;
 public class TestCountDownLatch {
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
+
 		final int N = 5;
-		final CountDownLatch startSignal = new CountDownLatch(1);
-		final CountDownLatch doneSignal = new CountDownLatch(N);
+		final CountDownLatch startLatch = new CountDownLatch(1);
+		final CountDownLatch finishLatch = new CountDownLatch(N);
 		final Random r = new Random();
+
 		for (int i = 0; i < N; ++i) {
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						startSignal.await();
-						System.out.println(Thread.currentThread().getName() + " starts!");
-						Thread.sleep(r.nextInt(5000));
-						System.out.println(Thread.currentThread().getName() + " done!");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					doneSignal.countDown();
+			new Thread(() -> {
+				try {
+					startLatch.await();
+					System.out.println(Thread.currentThread().getName() + " starts!");
+					Thread.sleep(r.nextInt(5000));
+					System.out.println(Thread.currentThread().getName() + " done!");
+					finishLatch.countDown();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}).start();
 		}
+
 		System.out.println("All workers start!");
-		startSignal.countDown();
-		try {
-			doneSignal.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		startLatch.countDown();
+		finishLatch.await();
 		System.out.println("All workers done!");
+
 	}
 
 }
