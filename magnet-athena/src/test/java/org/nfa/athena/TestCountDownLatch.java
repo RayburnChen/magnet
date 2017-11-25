1de90be2058b14593bf34dca256feb4b82a5c1be
OwenChen <OwenChen@windows10.microdone.cn>
2017-11-25 13:53:40 +0800

    add comments for CountDownLatch

--- magnet-athena/src/test/java/org/nfa/athena/TestCountDownLatch.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestCountDownLatch.java
@@ -2,6 +2,7 @@ package org.nfa.athena;
 
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
@@ -9,8 +10,41 @@ public class TestCountDownLatch {
 
 	@Test
 	public void test() throws InterruptedException {
+		
+//				CountDownLatch(1) AQS Node [thread,waitStatus]
+//
+//				count 1
+//
+//				t1 add
+//				t1 park
+//				[null,-1], [t1,0]
+//
+//				t2 add
+//				t2 park
+//				[null,-1], [t1,-1], [t2,0]
+//
+//				t3 add
+//				t3 park
+//				[null,-1], [t1,-1], [t2,-1], [t3,0]
+//
+//				count 0
+//
+//				[null,0], [t1,-1], [t2,-1], [t3,0]
+//
+//				t1 unpark
+//				[null,0], [t2,-1], [t3,0]
+//				t1 run
+//
+//				t2 unpark
+//				[null,0], [t3,0]
+//				t2 run
+//
+//				t3 unpark
+//				[null,0]
+//				t3 run
+		
 
-		final int N = 5;
+		final int N = 3;
 		final CountDownLatch startLatch = new CountDownLatch(1);
 		final CountDownLatch finishLatch = new CountDownLatch(N);
 		final Random r = new Random();
@@ -20,7 +54,7 @@ public class TestCountDownLatch {
 				try {
 					startLatch.await();
 					System.out.println(Thread.currentThread().getName() + " starts!");
-					Thread.sleep(r.nextInt(5000));
+					TimeUnit.MILLISECONDS.sleep(r.nextInt(5000));
 					System.out.println(Thread.currentThread().getName() + " done!");
 					finishLatch.countDown();
 				} catch (InterruptedException e) {
@@ -29,11 +63,29 @@ public class TestCountDownLatch {
 			}).start();
 		}
 
+		TimeUnit.SECONDS.sleep(1);
 		System.out.println("All workers start!");
 		startLatch.countDown();
 		finishLatch.await();
 		System.out.println("All workers done!");
 
 	}
+	
+
+	// CountDownLatch.await()
+	// When CountDownLatch count reach zero, it stop decrease.
+	// If CountDownLatch count == 0 then CountDownLatch.await() will pass immediately
+	// If CountDownLatch count > 0 then CountDownLatch.await() will add Node to AQS and park thread
+	
+	// CountDownLatch.countDown()
+	// If CountDownLatch count > 0 then CountDownLatch.countDown() will decrease count by 1
+	// If CountDownLatch count == 0 already then CountDownLatch.countDown() will do nothing
+	// When CountDownLatch.countDown() only one thread decrease and transition to zero will cause AQS.doReleaseShared()
+	
+	// AQS.doReleaseShared()
+	// Only once and signals successor and ensures propagation
+	// Set head Node waitStatus -1 -> 0 and unpark successor Node
+	// Recursive set new head and propagate
+	
 
 }
