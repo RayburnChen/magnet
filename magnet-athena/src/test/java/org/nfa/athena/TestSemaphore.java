8ddc9f8fa1a8c3395c906e785d431db7e5f1cad1
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-11-28 16:49:19 +0800

    add TestSemaphore

--- magnet-athena/src/test/java/org/nfa/athena/TestSemaphore.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestSemaphore.java
@@ -1,14 +1,47 @@
 package org.nfa.athena;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 public class TestSemaphore {
 
+	private final int count = 5;
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		Semaphore semaphore = new Semaphore(3);
+
+		List<Thread> list = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			list.add(new Thread(() -> {
+				try {
+					semaphore.acquire();
+					System.out.println(Thread.currentThread().toString() + " start");
+					TimeUnit.SECONDS.sleep(5);
+					System.out.println(Thread.currentThread().toString() + " done");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					semaphore.release();
+				}
+			}));
+		}
+
+		list.forEach(t -> t.start());
+
+		list.forEach(t -> {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+
 	}
 
 }
