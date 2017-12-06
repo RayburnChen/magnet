0e4a97f5190c703a3901073b8579c5d35c0475b4
OwenChen <OwenChen@windows10.microdone.cn>
2017-12-06 21:44:55 +0800

    add some comments

--- magnet-athena/src/main/java/org/nfa/athena/service/ApplicationService.java
+++ magnet-athena/src/main/java/org/nfa/athena/service/ApplicationService.java
@@ -1,5 +1,6 @@
 package org.nfa.athena.service;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Service
@@ -17,9 +19,18 @@ public class ApplicationService implements ApplicationListener<ContextRefreshedE
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent event) {
 		log.info("ApplicationService onApplicationEvent ContextRefreshedEvent" + event.getSource().getClass());
+		// org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext
 		Map<String, Object> beans = event.getApplicationContext().getBeansWithAnnotation(RestController.class);
 		// {athenaController : org.nfa.athena.controller.AthenaController}
-		beans.forEach((k, v) -> log.info("ApplicationService name:{} value:{}", k, v));
+		beans.forEach((k, v) -> {
+			log.info("ApplicationService name:{}, value:{}", k, v);
+			for (Method m : v.getClass().getDeclaredMethods()) {
+				RequestMapping anno = m.getAnnotation(RequestMapping.class);
+				if (null != anno) {
+					log.info("ApplicationService {} Method:{}, Path:{}", k, anno.method(), anno.value());
+				}
+			}
+		});
 	}
 
 }
---
+++ magnet-athena/src/main/java/org/nfa/athena/util/ApplicationContextUtils.java
@@ -0,0 +1,46 @@
+package org.nfa.athena.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Service;
+
+/**
+ * Starting Servlet Engine: Apache Tomcat
+ * Refreshing org.springframework.context.annotation.AnnotationConfigApplicationContext
+ * Fetching config
+ * Refreshing org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext
+ * Initializing Spring embedded WebApplicationContext
+ * Root WebApplicationContext: initialization completed
+ * Beans start to load
+ * 1. @PostConstruct
+ * 2. InitializingBean.afterPropertiesSet()
+ * 3. ApplicationContextAware
+ * 4. Registering Other Beans
+ * 5. ApplicationListener
+ * Tomcat started
+ * Started MagnetAthenaApplication
+ * 
+ * @author OwenChen
+ *
+ */
+@Service
+public class ApplicationContextUtils implements ApplicationContextAware {
+
+	private static ApplicationContext applicationContext;
+
+	private ApplicationContextUtils() {
+		super();
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext
+		ApplicationContextUtils.applicationContext = applicationContext;
+	}
+
+	public static Object getBean(String name) {
+		return applicationContext.getBean(name);
+	}
+
+}
