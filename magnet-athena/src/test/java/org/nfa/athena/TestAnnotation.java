c93025a34180d50f5600a6b3041ac083d994531e
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-12-13 16:44:42 +0800

    add testLoad

--- magnet-athena/src/test/java/org/nfa/athena/TestAnnotation.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestAnnotation.java
@@ -1,12 +1,16 @@
 package org.nfa.athena;
 
+import java.util.ServiceLoader;
+
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.Module;
 
 public class TestAnnotation {
 
 	@Test
 	public void test() throws ClassNotFoundException {
-		
+
 		Class<?> clazz = TestAnnotation.class.getClassLoader().loadClass("org.nfa.athena.service.AsyncService");
 		System.out.println("Load " + clazz);
 		if (clazz.isAnnotationPresent(EnableMagnetService.class)) {
@@ -16,6 +20,11 @@ public class TestAnnotation {
 			System.out.println("@EnableMagnetService value " + anno.value());
 		}
 
+	}
+
+	@Test
+	public void testLoad() {
+		ServiceLoader.load(Module.class, TestAnnotation.class.getClassLoader()).forEach(m -> System.out.println(m));
 	}
 
 }
