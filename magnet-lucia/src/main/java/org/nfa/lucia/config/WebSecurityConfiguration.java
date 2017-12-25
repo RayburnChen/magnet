913d01e7b93434c47fb5dc375cd323945ca94cb2
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-12-25 10:27:00 +0800

    disable oauth

--- magnet-lucia/src/main/java/org/nfa/lucia/config/WebSecurityConfiguration.java
+++ magnet-lucia/src/main/java/org/nfa/lucia/config/WebSecurityConfiguration.java
@@ -1,25 +1,15 @@
 package org.nfa.lucia.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
+@Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void init(WebSecurity web) {
 		web.ignoring().antMatchers("/**");
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// http.antMatcher("/**").authorizeRequests().anyRequest().authenticated();
-		// http.antMatcher("/**").authorizeRequests()
-		// .antMatchers("/greeting/oneUserByName").permitAll()
-		// .anyRequest().authenticated();
-
 	}
 
 }
