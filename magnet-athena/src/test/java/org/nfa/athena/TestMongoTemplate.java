c4fc392809915a1482e7507d19ae17210666eb2f
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2017-12-27 13:17:20 +0800

    add testProject

--- magnet-athena/src/test/java/org/nfa/athena/TestMongoTemplate.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestMongoTemplate.java
@@ -1,12 +1,16 @@
 package org.nfa.athena;
 
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.script.ExecutableMongoScript;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,12 +33,31 @@ public class TestMongoTemplate {
 		User user = new User();
 		user.setName("peter");
 		user.setUserType(UserType.ADMIN);
-//		EnumSet<UserType> set = EnumSet.allOf(UserType.class);
+		// EnumSet<UserType> set = EnumSet.allOf(UserType.class);
 		mongoTemplate.insert(user);
 		List<User> datas = mongoTemplate.findAll(User.class);
-		datas.forEach(u -> {
-			System.out.println(u);
-		});
+		datas.forEach(System.out::println);
+	}
+
+	@Test
+	public void testProject() {
+		Query q = new Query().withHint("document_update_history_true_idx");
+		projectFields(q.fields(), User.class);
+		List<User> datas = mongoTemplate.find(q, User.class);
+		datas.forEach(System.out::println);
+	}
+
+	private <V> void projectFields(org.springframework.data.mongodb.core.query.Field fs, Class<V> clazz) {
+		classIterator(fs, clazz);
+	}
+
+	private <V> void classIterator(org.springframework.data.mongodb.core.query.Field fs, Class<V> clazz) {
+		Arrays.stream(clazz.getDeclaredFields())
+				.filter(f -> !f.isAnnotationPresent(Transient.class) && !Modifier.isStatic(f.getModifiers()))
+				.map(f -> f.getName()).forEach(f -> fs.include(f));
+		if (null != clazz.getSuperclass()) {
+			classIterator(fs, clazz.getSuperclass());
+		}
 	}
 
 }
