151eb5c36e64df8d7cb1a03ed7640449f965a843
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2018-01-05 16:19:49 +0800

    add opsForList

--- magnet-athena/src/test/java/org/nfa/athena/TestRedisTemplate.java
+++ magnet-athena/src/test/java/org/nfa/athena/TestRedisTemplate.java
@@ -1,5 +1,7 @@
 package org.nfa.athena;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,18 @@ public class TestRedisTemplate {
 	private StringRedisTemplate stringRedisTemplate;
 
 	@Test
-	public void test() {
-		stringRedisTemplate.opsForValue().set("001", "user");
+	public void opsForValue() {
+		stringRedisTemplate.opsForValue().set("key01", "user01");
 		System.err.println("StringRedisTemplate opsForValue " + stringRedisTemplate.opsForValue().get("001"));
 		System.err.println("StringRedisTemplate opsForValue " + stringRedisTemplate.opsForValue().get("002"));
+	}
+
+	@Test
+	public void opsForList() {
+		stringRedisTemplate.opsForList().rightPush("list01", "user01");
+		long end = stringRedisTemplate.opsForList().size("list01");
+		List<String> list = stringRedisTemplate.opsForList().range("list01", 0, end);
+		System.err.println(list);
 	}
 
 }
