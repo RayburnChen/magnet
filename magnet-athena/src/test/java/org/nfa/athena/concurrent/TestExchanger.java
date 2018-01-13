d2b2676bb25a9987363103fde184340781f733a7
cccchenccc <cccchenccc@163.com>
2018-01-13 20:47:47 +0800

    add TestExchanger

--- magnet-athena/src/test/java/org/nfa/athena/concurrent/TestExchanger.java
+++ magnet-athena/src/test/java/org/nfa/athena/concurrent/TestExchanger.java
@@ -1,14 +1,36 @@
 package org.nfa.athena.concurrent;
 
+import java.util.Random;
 import java.util.concurrent.Exchanger;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 public class TestExchanger {
 
+	private static final Random RAN = new Random();
+	private static final int THREAD_TOTAL = 10;
+
 	@Test
-	public void test() {
-		Exchanger exchanger = new Exchanger();
+	public void testExchanger() throws InterruptedException {
+		// swap elements within pairs
+		// just like exchange name card
+		Exchanger<String> exchanger = new Exchanger<String>();
+		for (int i = 0; i < THREAD_TOTAL; i++) {
+			new Thread(() -> {
+				try {
+					System.out.println(Thread.currentThread().toString() + " start exchange");
+					TimeUnit.MILLISECONDS.sleep(RAN.nextInt(1000));
+					String feedback = exchanger.exchange(Thread.currentThread().toString());
+					System.out.println(Thread.currentThread().toString() + " feedback " + feedback);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+
+		TimeUnit.SECONDS.sleep(5L);
+		
 	}
 
 }
