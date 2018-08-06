1208b7a82e6a6cda6bbd45b43f49abb2d106c5eb
cccchenccc <cccchenccc@163.com>
2018-08-06 22:40:48 +0800

    add spring sleuth

--- magnet-zipkin/src/main/java/org/nfa/zipkin/MagnetZipkinApplication.java
+++ magnet-zipkin/src/main/java/org/nfa/zipkin/MagnetZipkinApplication.java
@@ -2,13 +2,18 @@ package org.nfa.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
+// @EnableZipkinStreamServer
 public class MagnetZipkinApplication {
+
+	// https://cloud.spring.io/spring-cloud-static/Dalston.SR4/multi/multi__span_data_as_messages.html#_zipkin_consumer
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetZipkinApplication.class, args);
 	}
+
 }
