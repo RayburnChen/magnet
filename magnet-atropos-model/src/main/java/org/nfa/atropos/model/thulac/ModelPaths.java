7f541dc3091eef799c52bda297926e153d777ea1
cccchenccc <cccchenccc@163.com>
2018-08-07 21:19:33 +0800

    add thulac

---
+++ magnet-atropos-model/src/main/java/org/nfa/atropos/model/thulac/ModelPaths.java
@@ -0,0 +1,14 @@
+package org.nfa.atropos.model.thulac;
+
+/**
+ * 模型文件路径名
+ */
+public final class ModelPaths {
+
+	public static final String NS_BIN_PATH = "/thulac/ns_dat.bin";
+	public static final String IDIOM_BIN_PATH = "/thulac/idiom_dat.bin";
+	public static final String LABEL_PATH = "/thulac/model_c_label.txt";
+	public static final String WEIGHT_PATH = "/thulac/model_c_model.bin";
+	public static final String FEATURE_PATH = "/thulac/model_c_dat.bin";
+
+}
