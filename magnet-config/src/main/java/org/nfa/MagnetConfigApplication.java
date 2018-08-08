b6540a85aeccaab0b6cbe1150e628b5ecc8e8d89
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2018-08-08 14:17:40 +0800

    update yml

--- magnet-config/src/main/java/org/nfa/MagnetConfigApplication.java
+++ magnet-config/src/main/java/org/nfa/MagnetConfigApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class MagnetConfigApplication {
 
+	// http://localhost:8888/magnet-athena/dev
+
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetConfigApplication.class, args);
 	}
