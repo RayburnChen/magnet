7a61800dd07dae7d24dc312f9a9471dae4c648f0
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2018-08-09 12:43:43 +0800

    update gateway routes

--- magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
+++ magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
@@ -2,17 +2,27 @@ package org.nfa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MagnetGatewayApplication {
 
+	// http://localhost:8080/actuator/gateway/routes
+	// http://localhost:8080/MAGNET-ATHENA/greeting/oneUser
+	// http://localhost:8080/athena/greeting/oneUser
+
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetGatewayApplication.class, args);
 	}
 
-	// http://localhost:8080/magnet-gateway/v1/athena/magnet-athena/greeting/oneUser
-	// http://localhost:8080/magnet-gateway/v1/lucia/magnet-lucia/athenaUser
+	@Bean
+	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties);
+	}
 
 }
