1e9764d7437be0c317a1c8f7dde386d8d53c78a4
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2018-08-09 11:12:10 +0800

    fix gateway

--- magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
+++ magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
@@ -2,8 +2,10 @@ package org.nfa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MagnetGatewayApplication {
 
 	public static void main(String[] args) {
