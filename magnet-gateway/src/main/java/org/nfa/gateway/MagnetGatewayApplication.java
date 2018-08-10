310b8642ce986c48d8f2600bd30f434685797045
cccchenccc <cccchenccc@163.com>
2018-08-10 20:59:40 +0800

    add routes for gateway

--- magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
+++ magnet-gateway/src/main/java/org/nfa/gateway/MagnetGatewayApplication.java
@@ -2,14 +2,12 @@ package org.nfa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class MagnetGatewayApplication {
 
 	// http://localhost:8080/actuator/gateway/routes
@@ -18,11 +16,6 @@ public class MagnetGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MagnetGatewayApplication.class, args);
-	}
-
-	@Bean
-	public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient, DiscoveryLocatorProperties discoveryLocatorProperties) {
-		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, discoveryLocatorProperties);
 	}
 
 }
