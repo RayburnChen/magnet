634a39e52c083551e62dced037e8569bb2c70dbb
ochen033 <owen.chen@pwc.com>
2018-08-13 16:42:42 +0800

    add error handle for zuul

--- magnet-zuul/src/main/java/org/nfa/zuul/MagnetZuulApplication.java
+++ magnet-zuul/src/main/java/org/nfa/zuul/MagnetZuulApplication.java
@@ -1,15 +1,23 @@
 package org.nfa.zuul;
 
+import org.nfa.panel.config.GlobalErrorController;
+import org.nfa.panel.config.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableMongoAuditing
+@EnableMongoRepositories
+@Import(value = { GlobalErrorController.class, GlobalExceptionHandler.class })
 public class MagnetZuulApplication {
 
 	// http://localhost:8080/actuator/routes
