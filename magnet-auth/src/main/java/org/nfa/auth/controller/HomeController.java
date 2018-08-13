b039a0c204a551f41c5fc07223c3d6a2bf5066bb
ochen033 <ochen033@ochen03316.us.nam.ad.pwcinternal.com>
2018-08-13 13:12:19 +0800

    polish code

--- magnet-auth/src/main/java/org/nfa/auth/controller/HomeController.java
+++ magnet-auth/src/main/java/org/nfa/auth/controller/HomeController.java
@@ -1,11 +1,10 @@
 package org.nfa.auth.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
 public class HomeController {
 
 	@RequestMapping("/login")
