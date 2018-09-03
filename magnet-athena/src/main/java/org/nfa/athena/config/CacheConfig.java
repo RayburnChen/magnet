ddbbbff46cf4c0a6018794401f5a26bab2faefa4
ochen033 <owen.chen@pwc.com>
2018-09-03 13:06:02 +0800

    fix cache config

--- magnet-athena/src/main/java/org/nfa/athena/config/CacheConfig.java
+++ magnet-athena/src/main/java/org/nfa/athena/config/CacheConfig.java
@@ -2,14 +2,20 @@ package org.nfa.athena.config;
 
 import java.util.TimeZone;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,18 +25,23 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
+@EnableConfigurationProperties(CacheProperties.class)
 public class CacheConfig {
 
 	public static final String USER_CACHE = "USER_CACHE";
 	public static final String USER_NAME_CACHE = "USER_NAME_CACHE";
 
+	@Autowired
+	private CacheProperties cacheProperties;
+
 	@Bean
-	public CacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+	public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
 		// configure and return an implementation of Spring's CacheManager SPI
-		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-		cacheManager.setCachePrefix(s -> s.concat(":").getBytes());
-		cacheManager.setUsePrefix(true);
-		return cacheManager;
+		RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().entryTtl(cacheProperties.getRedis().getTimeToLive()).disableCachingNullValues()
+				.computePrefixWith(cacheName -> cacheProperties.getRedis().getKeyPrefix() + cacheName + ":")
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+		return RedisCacheManager.builder(connectionFactory).cacheDefaults(defaultCacheConfig).transactionAware().build();
 	}
 
 	@Bean
