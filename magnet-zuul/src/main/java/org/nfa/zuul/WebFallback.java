c6d7db900396947503592d2edd9c52eeea63ec33
ochen033 <owen.chen@pwc.com>
2018-09-19 09:41:55 +0800

    add WebFallback

---
+++ magnet-zuul/src/main/java/org/nfa/zuul/WebFallback.java
@@ -0,0 +1,74 @@
+package org.nfa.zuul;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.nfa.base.ErrorResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class WebFallback implements FallbackProvider {
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Override
+	public String getRoute() {
+		return "*";
+	}
+
+	public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+		return new ClientHttpResponse() {
+			private HttpStatus status = HttpStatus.BAD_GATEWAY;
+
+			@Override
+			public HttpStatus getStatusCode() throws IOException {
+				return status;
+			}
+
+			@Override
+			public int getRawStatusCode() throws IOException {
+				return status.value();
+			}
+
+			@Override
+			public String getStatusText() throws IOException {
+				return status.name();
+			}
+
+			@Override
+			public void close() {
+
+			}
+
+			@Override
+			public InputStream getBody() throws IOException {
+				ErrorResponse error = new ErrorResponse();
+				error.setCode(10000);
+				error.setPath(route);
+				error.setMessage(cause.getMessage());
+				error.setException(cause.getClass().getSimpleName());
+				ResponseEntity<ErrorResponse> response = new ResponseEntity<ErrorResponse>(error, HttpStatus.SERVICE_UNAVAILABLE);
+				return new ByteArrayInputStream(objectMapper.writeValueAsBytes(response));
+			}
+
+			@Override
+			public HttpHeaders getHeaders() {
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				return headers;
+			}
+		};
+	}
+
+}
