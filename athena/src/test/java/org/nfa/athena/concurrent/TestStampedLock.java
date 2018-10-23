db63f23a5ac8ee3472922bd953aaf3e97a57d8d4
ochen033 <owen.chen@pwc.com>
2018-10-23 12:52:58 +0800

    TestStampedLock

--- athena/src/test/java/org/nfa/athena/concurrent/TestStampedLock.java
+++ athena/src/test/java/org/nfa/athena/concurrent/TestStampedLock.java
@@ -1,20 +1,48 @@
 package org.nfa.athena.concurrent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.StampedLock;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestStampedLock {
+
+	private static final Logger log = LoggerFactory.getLogger(TestStampedLock.class);
 
 	private double x, y;
 	private final StampedLock sl = new StampedLock();
 
+	// 0000,0000,0001 RUNIT
+	// 0000,1000,0000 WBIT
+	// 0000,1111,1111 ABITS
+	// 1111,0111,1111 SBITS
+
 	@Test
 	public void testStampedLock() {
+		List<CompletableFuture<Void>> futures = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			futures.add(CompletableFuture.runAsync(() -> oneStep()));
+		}
+		CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
 	}
 
-	private void move(double deltaX, double deltaY) { 
-		long stamp = sl.writeLock();// an exclusively locked method
+	private void oneStep() {
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+		moveIfAtOrigin(random.nextDouble(), random.nextDouble());
+		move(random.nextDouble(), random.nextDouble());
+		distanceFromOrigin();
+	}
+
+	private void move(double deltaX, double deltaY) { // an exclusively locked method
+		// 0001,0000,0000 initial state
+		// 0001,1000,0000 writeLock
+		// 0010,0000,0000 unlockWrite
+		long stamp = sl.writeLock();
 		try {
 			x += deltaX;
 			y += deltaY;
@@ -23,7 +51,11 @@ public class TestStampedLock {
 		}
 	}
 
-	private double distanceFromOrigin() {
+	private double distanceFromOrigin() { // A read-only method
+		// 0001,0000,0000 initial state
+		// 0001,0000,0000 tryOptimisticRead
+		// 0001,0000,0001 readLock
+		// 0001,0000,0000 unlockRead
 		long stamp = sl.tryOptimisticRead();
 		double currentX = x, currentY = y;
 		if (!sl.validate(stamp)) {
@@ -35,15 +67,28 @@ public class TestStampedLock {
 				sl.unlockRead(stamp);
 			}
 		}
-		return Math.sqrt(currentX * currentX + currentY * currentY);
+		double dis = Math.sqrt(currentX * currentX + currentY * currentY);
+		log.info("name {} distance {}", Thread.currentThread().toString(), dis);
+		return dis;
 	}
 
-	private void moveIfAtOrigin(double newX, double newY) {
+	private void moveIfAtOrigin(double newX, double newY) { // upgrade
 		// Could instead start with optimistic, not read mode
+		// 0001,0000,0000 initial state
+		// 0001,0000,0001 readLock
+		// 0001,1000,0000 tryConvertToWriteLock
+		// 0010,0000,0000 unlock
+
+		// 0001,0000,0000 initial state
+		// 0001,0000,0001 readLock
+		// 0000,0000,0000 tryConvertToWriteLock failed
+		// 0001,0000,0000 unlockRead
+		// 0001,1000,0000 writeLock
+		// 0010,0000,0000 unlock
 		long stamp = sl.readLock();
 		try {
 			while (x == 0.0 && y == 0.0) {
-				long ws = sl.tryConvertToWriteLock(stamp); // upgrade
+				long ws = sl.tryConvertToWriteLock(stamp);
 				if (ws != 0L) {
 					stamp = ws;
 					x = newX;
@@ -58,5 +103,4 @@ public class TestStampedLock {
 			sl.unlock(stamp);
 		}
 	}
-
 }
