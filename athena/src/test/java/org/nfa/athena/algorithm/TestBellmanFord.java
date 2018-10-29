98500ea3d89c0fb05ee57541470ccd494c03003c
cccchenccc <cccchenccc@163.com>
2018-10-29 21:49:20 +0800

    update TestBellmanFord

--- athena/src/test/java/org/nfa/athena/algorithm/TestBellmanFord.java
+++ athena/src/test/java/org/nfa/athena/algorithm/TestBellmanFord.java
@@ -1,34 +1,106 @@
 package org.nfa.athena.algorithm;
 
-import java.util.Arrays;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestBellmanFord {
+	
+//	From 0 to 0 Weight 0 Routes 0
+//	From 0 to 1 Weight 3 Routes 0->3->1
+//	From 0 to 2 Weight 3 Routes 0->3->2
+//	From 0 to 3 Weight 2 Routes 0->3
+//	From 0 to 4 Weight 6 Routes 0->3->2->4
 
 	@Test
 	public void testGraph1() {
-		int m = Integer.MAX_VALUE, start = 0;
+		int m = Integer.MAX_VALUE / 10, start = 0;
 		int[][] graph = { 
 				{ 0, 4, m, 2, m }, 
 				{ 4, 0, 4, 1, m }, 
 				{ m, 4, 0, 1, 3 }, 
 				{ 2, 1, 1, 0, 7 },
 				{ m, m, 3, 7, 0 } };
-		Assert.assertArrayEquals(new int[] { 0, Integer.MAX_VALUE, 10, 50, 30, 60 }, bellmanFord(graph, start));
+		Assert.assertArrayEquals(new int[] { 0, 3, 3, 2, 6 }, bellmanFord(graph, start));
+	}
+	
+//	From 0 to 0 Weight 0 Routes 0
+//	From 0 to 1 Weight -1 Routes 0->1
+//	From 0 to 2 Weight 2 Routes 0->1->2
+//	From 0 to 3 Weight -2 Routes 0->1->4->3
+//	From 0 to 4 Weight 1 Routes 0->1->4
+	
+	@Test
+	public void testGraph2() {
+		int m = Integer.MAX_VALUE / 10, start = 0;
+		int[][] graph = { 
+				{ 0, -1, 4, m,  m }, 
+				{ m, 0,  3, 2,  2 }, 
+				{ m, m,  0, m,  m }, 
+				{ m, 1,  5, 0,  m },
+				{ m, m,  m, -3, 0 } };
+		Assert.assertArrayEquals(new int[] { 0, -1, 2, -2, 1 }, bellmanFord(graph, start));
+	}
+	
+	@Test
+	public void testGraph3() {
+		int m = Integer.MAX_VALUE / 10, start = 0;
+		int[][] graph = { 
+				{ 0, -1, 4, m,  m }, 
+				{ m, 0,  3, 2,  2 }, 
+				{ m, m,  0, m,  m }, 
+				{ m, 1,  5, 0,  m },
+				{ m, m,  m, -5, 0 } };
+		try {
+			bellmanFord(graph, start);
+			Assert.fail();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private int[] bellmanFord(int[][] graph, int start) {
+		GraphContext context = bellmanFord(new GraphContext(graph, start));
+		for (int i = 0; i < context.size; i++) {
+			System.out.println(
+					"From " + start + " to " + i + " Weight " + context.weights[i] + " Routes " + context.routes[i]);
+		}
+		return context.weights;
+	}
 
-		return null;
+	private GraphContext bellmanFord(GraphContext c) {
+		int[] weights = c.weights;
+		for (int k = 0; k < c.size - 1; k++) {
+			for (int i = 0; i < c.size; i++) {
+				for (int j = 0; j < c.size; j++) {
+					if (i == j || c.graph[i][j] == Integer.MAX_VALUE / 10) {
+						continue;// skip
+					}
+					int wei = weights[i] + c.graph[i][j];
+					if (wei < weights[j]) {
+						weights[j] = wei;
+						c.routes[j] = c.routes[i] + "->" + j;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < c.size; i++) {
+			for (int j = 0; j < c.size; j++) {
+				if (i == j || c.graph[i][j] == Integer.MAX_VALUE / 10) {
+					continue;// skip
+				}
+				int wei = weights[i] + c.graph[i][j];
+				if (wei < weights[j]) {
+					System.err.println("存在负权回路");
+					throw new RuntimeException("存在负权回路");
+				}
+			}
+		}
+		return c;
 	}
 
 	private class GraphContext {
-		private int start;
 		private final int[][] graph;
 		private final int size;
-		private final boolean[] visited;
 		private final int[] weights;
 		private final String[] routes;
 
@@ -36,15 +108,14 @@ public class TestBellmanFord {
 			super();
 			int n = graph.length;
 			this.graph = graph;
-			this.start = start;
 			this.size = n;
-			this.weights = Arrays.copyOf(graph[start], n);
+			this.weights = new int[n];
 			this.routes = new String[n];
 			for (int i = 0; i < n; i++) {
-				this.routes[i] = start + "->" + i;
+				this.weights[i] = Integer.MAX_VALUE / 10;
+				this.routes[i] = String.valueOf(start);
 			}
-			this.visited = new boolean[n];
-			this.visited[start] = true;
+			weights[start] = 0;
 		}
 
 	}
